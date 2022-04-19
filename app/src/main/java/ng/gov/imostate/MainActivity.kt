506937f944da23170261836152fa85eedc413db1
Hassan Abdulwahab <devhassan.org@gmail.com>
2022-04-19 14:41:09 +0100

    add bottom nav

--- app/src/main/java/ng/gov/imostate/MainActivity.kt
+++ app/src/main/java/ng/gov/imostate/MainActivity.kt
@@ -2,10 +2,19 @@ package ng.gov.imostate
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ng.gov.imostate.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    //view binding
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.bottomNavigationView.background = null
     }
 }

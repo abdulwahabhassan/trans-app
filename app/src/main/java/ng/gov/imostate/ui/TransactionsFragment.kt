6d98ba756856da168a770cc1772ebebba2310567
Hassan Abdulwahab <devhassan.org@gmail.com>
2022-05-01 00:53:33 +0100

    consume get transactions endpoint

--- app/src/main/java/ng/gov/imostate/ui/TransactionsFragment.kt
+++ app/src/main/java/ng/gov/imostate/ui/TransactionsFragment.kt
@@ -5,12 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.*
 import androidx.navigation.fragment.findNavController
 import dagger.hilt.android.AndroidEntryPoint
 import ng.gov.imostate.util.Mock
 import ng.gov.imostate.model.domain.Transaction
 import ng.gov.imostate.adapter.TransactionsAdapter
 import ng.gov.imostate.databinding.FragmentTransactionsBinding
+import ng.gov.imostate.model.result.ViewModelResult
+import ng.gov.imostate.util.AppUtils
+import ng.gov.imostate.viewmodel.AppViewModelsFactory
+import ng.gov.imostate.viewmodel.HomeFragmentViewModel
+import ng.gov.imostate.viewmodel.TransactionsFragmentViewModel
+import timber.log.Timber
+import www.sanju.motiontoast.MotionToastStyle
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class TransactionsFragment : Fragment() {
@@ -18,6 +27,10 @@ class TransactionsFragment : Fragment() {
     private var _binding: FragmentTransactionsBinding? = null
     private val binding get() = _binding!!
     private lateinit var transactionsAdapter: TransactionsAdapter
+    @Inject
+    lateinit var appViewModelFactory: AppViewModelsFactory
+    //view model
+    private lateinit var viewModel: TransactionsFragmentViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,6 +49,11 @@ class TransactionsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         binding.searchView.queryHint = "Search Transactions"
 
+        viewModel = ViewModelProvider(
+            this,
+            appViewModelFactory
+        ).get(TransactionsFragmentViewModel::class.java)
+
         initRV()
 
         initUI()
@@ -49,7 +67,23 @@ class TransactionsFragment : Fragment() {
     }
 
     private fun initUI() {
-        transactionsAdapter.submitList(Mock.getTransactions())
+
+        viewLifecycleOwner.lifecycleScope.launchWhenResumed {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.RESUMED) {
+                val result = viewModel.getInitialUserPreferences().token?.let { token ->
+                    viewModel.getTransactions(token)
+                }!!
+                when (result) {
+                    is ViewModelResult.Success -> {
+                        Timber.d("${result.data?.transactions}")
+                        transactionsAdapter.submitList(Mock.getTransactions())
+                    }
+                    is ViewModelResult.Error -> {
+                        AppUtils.showToast(requireActivity(), result.errorMessage, MotionToastStyle.ERROR)
+                    }
+                }
+            }
+        }
 
         with(binding) {
             backArrowIV.setOnClickListener {
